61ce0435f6d99e31672fcb176eddcfb16e1081c5
Jan Verbeek <jverbeek@therp.nl>
2020-08-06 10:37:35 +0200

    fixup! [IMP] Refactor and debug Let's Encrypt ACMEv2 support

--- letsencrypt/models/base_config_settings.py
+++ letsencrypt/models/base_config_settings.py
@@ -57,8 +57,8 @@ class BaseConfigSettings(models.TransientModel):
         string="Prefer DNS validation",
         help=(
             "Validate through DNS even when HTTP validation is possible. "
-            "Use this if your Odoo instance isn't publicly accessible.",
-        )
+            "Use this if your Odoo instance isn't publicly accessible."
+        ),
     )
 
     @api.onchange('letsencrypt_altnames', 'letsencrypt_prefer_dns')
